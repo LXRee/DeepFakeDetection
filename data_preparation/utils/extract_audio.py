44619b7d44886562f5e4c8ecb3a30247fe07b2a7
Matteo Rizzo <matteo.rizzo.work@gmail.com>
2020-03-01 21:22:54 -0800

    Fixed audio extraction

--- data_preparation/utils/extract_audio.py
+++ data_preparation/utils/extract_audio.py
@@ -1,38 +1,40 @@
 import multiprocessing as mp
 import os
+import shlex
 import subprocess
 
 from data_preparation.utils.make_chunks import list_chunks
 
+OS_TYPE = "linux"
+ABSOLUTE_PATH = "/home/matteo/Projects/Github/DeepFakeDetection"
+RELATIVE_INPUT_PATH = "dataset/deepfake-detection-challenge/train_sample_videos"
+RELATIVE_OUTPUT_PATH = "dataset/deepfake-detection-challenge/train_sample_extracted_audio"
+INPUT_PATH = os.path.join(ABSOLUTE_PATH, RELATIVE_INPUT_PATH)
+OUTPUT_PATH = os.path.join(ABSOLUTE_PATH, RELATIVE_OUTPUT_PATH)
 
-def extract_audio_from_paths(in_paths, dest_path):
-    for in_path in in_paths:
-        if os.path.splitext(os.path.basename(in_path))[1] == '.mp4':
-            wav_dest = os.path.join(dest_path, os.path.basename(in_path).split('.')[0])
-            command = "ffmpeg.exe -i {} -ab 160K -ac 1 -ar 44100 -vn {}.wav".format(in_path, wav_dest)
-            # in order to suppress output and to not overload processor with it
-            p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def extract_audio_from_chunk(file_paths: list):
+    for file_path in file_paths:
+        if os.path.splitext(os.path.basename(file_path))[1] == '.mp4':
+            print("Processing {}...".format(os.path.splitext(os.path.basename(file_path))[0]))
+            wav_dest = os.path.join(OUTPUT_PATH, os.path.basename(file_path).split('.')[0])
+            main_cmd = "ffmpeg.exe" if OS_TYPE == "windows" else "ffmpeg"
+            cmd = main_cmd + " -i {} -ab 160K -ac 1 -ar 44100 -vn {}.wav".format(file_path, wav_dest)
+            p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             _ = p.communicate()
 
 
-def extract_audio_from_source(input_path, dest_path):
-    # Retrieve all paths in input_path folder
-    paths = []
-    for root, dirs, files in os.walk(input_path):
-        paths.extend([os.path.join(root, file) for file in files])
-
-    # Needed for multiprocessing
-    paths = list(paths)
-    processors = 6
+def extract_audios_from_videos():
+    paths = [os.path.join(INPUT_PATH, file_name) for file_name in os.listdir(INPUT_PATH)]
+    processors = mp.cpu_count()
     pool = mp.Pool(processes=processors)
-    arguments = list_chunks(paths, len(paths) // processors)
-    results = [pool.apply_async(extract_audio_from_paths, args=(a, dest_path)) for a in arguments]
+    paths_chunks = list_chunks(paths, len(paths) // processors)
+    results = [pool.apply_async(extract_audio_from_chunk, args=[chunk]) for chunk in paths_chunks]
     _ = [p.get() for p in results]
-
-    # extract_audio_from_paths(paths, dest_path)
 
 
 if __name__ == '__main__':
-    in_path = "../../dataset/deepfake-detection-challenge/train_sample_videos"
-    out_path = "../../dataset/deepfake-detection-challenge/train_sample_extracted_audio"
-    extract_audio_from_source(in_path, out_path)
+    print("\nExtracting WAW audio files from videos...\n")
+    extract_audios_from_videos()
+    print("\nExtracted {na}/{nv} WAW audio files!".format(na=len(os.listdir(OUTPUT_PATH)),
+                                                          nv=len(os.listdir(INPUT_PATH))))
